60825e027e4a7d811beeb81f12445ae2a4cd8671
Zachary Sutton <zachary-sutton@outlook.com>
2021-09-24 16:04:41 +0100

    removed hyperlink in app page

--- src/App.js
+++ src/App.js
@@ -10,14 +10,6 @@ function App() {
         <AdvertBanner />
         <img src={logo} className="App-logo" alt="logo" />
         <HomeContentCardContainer />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
