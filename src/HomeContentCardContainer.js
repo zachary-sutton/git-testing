5836122673dcd92b814d9abcaec1be4170225cf7
Zachary Sutton <zachary-sutton@outlook.com>
2021-09-24 12:41:47 +0100

    logic and display implemented for cards. fixed issue of not being able to map due to api loading time. implementing changes on correct branch instead of master

--- src/HomeContentCardContainer.js
+++ src/HomeContentCardContainer.js
@@ -1,12 +1,26 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import HomeContentCard from "./HomeContentCard"
 
 function HomeContentCardContainer(){
+
+    const [products, setProducts] = useState()
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products')
+            .then(res=>res.json())
+            .then(json=>setProducts(json.slice(1, 4)))
+    }, [])
+
+    //&& meeans if statement on left is false it won't even bother with the code on the right
+    //in this case when products is undefined it returns false
+    //when the fetch request is completed the component rerenders and products is populated, returning true - .map will now run
+    const mapItems = products && products.map(prod => <HomeContentCard name={prod.title} src={prod.image} desc={prod.description} key={prod.id} />)
+
+    console.log(products)
+
     return(
         <div className="home-content-card-container">
-            <HomeContentCard name="ssssssssss" desc="cool item hardcode" />
-            <HomeContentCard name="ffffff" desc="cool item hardcode" />
-            <HomeContentCard name="ddddddddd" desc="cool item hardcode" />
+            {mapItems}
         </div>
     )
 }
