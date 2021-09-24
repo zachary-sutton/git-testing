5fc7aeb46a658c574426b888ca001d0d9caa8481
Zachary Sutton <zachary-sutton@outlook.com>
2021-09-24 09:56:46 +0100

    added status update para on app.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <h1>
           nsdglkdsgnskjgnjsgkjsdngkjskjdgnk
         </h1>
+        <p>status change</p>
         <a
           className="App-link"
           href="https://reactjs.org"
